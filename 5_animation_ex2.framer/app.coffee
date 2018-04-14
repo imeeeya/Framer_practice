243001ef1a0e9653760f0540e4410f0aa0f3fc66
imee <imeeeeya@gmail.com>
2018-04-14 23:06:01 +0900

    animation practice example

---
+++ 5_animation_ex2.framer/app.coffee
@@ -0,0 +1,50 @@
+# psd file import
+psd = Framer.Importer.load("imported/animation@1x")
+# defalult setting
+psd.bg.scale = 1.2
+psd.bg.opacity = 0
+psd.logo.scale = 0.5
+psd.logo.opacity = 0
+psd.logo.midY = 667
+psd.text_welcome.opacity = 0
+psd.ui_btn.y = 1100
+psd.ui_btn.opacity = 0
+#background animation
+psd.bg.animate
+	scale : 1, opacity: 1
+#logo appeard
+psd.logo.animate	
+	scale: 1, opacity: 1
+	options: 	
+		time: 1
+		curve: "Spring(damping:0.40)"
+#logo move
+psd.logo.animate
+	y : 216
+	options: 	
+		delay: 1.5
+		curve: "Spring(damping: 0.66)"
+
+#animation1 = new Animation
+#make function 'animation1'
+animation1 = ->
+	psd.text_welcome.animate
+		opacity: 1
+		options :
+			delay : 2
+	psd.ui_btn.animate
+		y: 844, opacity: 1
+		options: 
+			curve: "ease"
+			time: 0.5
+			delay: 1.5
+#Start function	
+animation1();
+
+
+
+		
+	
+	
+	
+	
---
+++ 5_animation_ex2.framer/imported/animation@1x/layers.json.js
@@ -0,0 +1,128 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["animation/layers.json.js"] = [
+	{
+		"id": 42,
+		"name": "bg",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/bg.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 750,
+				"height": 1334
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1182142366"
+	},
+	{
+		"id": 21,
+		"name": "ui_btn",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/ui_btn.png",
+			"frame": {
+				"x": 104,
+				"y": 893,
+				"width": 541,
+				"height": 376
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1223885400"
+	},
+	{
+		"id": 44,
+		"name": "text_welcome",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/text_welcome.png",
+			"frame": {
+				"x": 223,
+				"y": 642,
+				"width": 302,
+				"height": 45
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1181129664"
+	},
+	{
+		"id": 40,
+		"name": "logo",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/logo.png",
+			"frame": {
+				"x": 223,
+				"y": 216,
+				"width": 301,
+				"height": 250
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "915485958"
+	},
+	{
+		"id": 38,
+		"name": "status",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/status.png",
+			"frame": {
+				"x": 11,
+				"y": 11,
+				"width": 726,
+				"height": 20
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1181099812"
+	}
+]
